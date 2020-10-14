4a69789e9012b4b6e703a98e436fcefb0d95423c
Junha Park <junha.park@mail.mcgill.ca>
2020-10-13 20:27:23 -0400

    added text intro

---
+++ project1/script.js
@@ -0,0 +1,38 @@
+var vid = document.getElementById("background-video");
+vid.playbackRate = 0.8;
+
+setTimeout(function() { show_youtube(); }, 5000);
+
+function show_youtube() {
+    document.getElementById('script-container').style.display = 'none';
+    importYoutube()
+    onYouTubeIframeAPIReady();
+    document.getElementById('youtube').style.margin = '3vh 0';
+}
+
+function importYoutube(){
+    var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+    var player;
+    player = new YT.Player('youtube', {
+        height: '390',
+        width: '660',
+        videoId: 'Wf_2Q2wRWes',
+        events: {
+        'onReady': onPlayerReady
+        }
+    });
+}
+    
+    // 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+    
