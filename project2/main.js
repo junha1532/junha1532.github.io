87f70ba467df5cc8e2b429ea0725db92fb989939
Junha Park <junha.park@mail.mcgill.ca>
2021-03-19 23:23:24 -0400

    Added photo swipper

--- project2/main.js
+++ project2/main.js
@@ -6,8 +6,11 @@ const languageSelector = document.querySelector('.language__selector');
 const navMenu = document.querySelector('.navbar__menu');
 const languageMenu = document.querySelector('.language__menu');
 const inquiryBtn = document.querySelector('.inquiry__button');
-const contactForm = document.querySelector('#contact-form');
+const inquiryForm = document.querySelector("#gform");
+const inquiryFormContainer = document.querySelector('#contact-form');
 const contactBtn = document.querySelector('.navbar__menu').children[3];
+const swiperCloseBtn = document.querySelector('#swiper-close-button');
+
 // const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () =>{
@@ -26,6 +29,13 @@ inquiryBtn.addEventListener('click', () =>{
 
 contactBtn.addEventListener('click', () =>{
   toggleContactForm();
+});
+
+// Sends Email when pressing inquiry send button
+
+inquiryForm.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  sendEmailInquiry();
 });
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -48,8 +58,8 @@ function toggleContactForm(){
     inquiryBtn.querySelector('i').classList.remove('unpressed');
     inquiryBtn.querySelector('i').classList.add('pressed');
     //Appear Contact Form
-    contactForm.classList.remove('unpressed');
-    contactForm.classList.add('pressed');
+    inquiryFormContainer.classList.remove('unpressed');
+    inquiryFormContainer.classList.add('pressed');
   }else{
     //Change icon
     inquiryBtn.querySelector('i').classList.remove('fas');
@@ -60,9 +70,60 @@ function toggleContactForm(){
     inquiryBtn.querySelector('i').classList.remove('pressed');
     inquiryBtn.querySelector('i').classList.add('unpressed');
     //Disappear Contact Form
-    contactForm.classList.add('unpressed');
+    inquiryFormContainer.classList.add('unpressed');
     setTimeout(() => {
-      contactForm.classList.remove('pressed');
+      inquiryFormContainer.classList.remove('pressed');
     }, 495)
   }
 }
+
+swiperCloseBtn.addEventListener('click', () =>{
+  document.querySelector("#photo-slider-wrapper").classList.remove('visible');
+});
+
+document.querySelector("#roomABtn").addEventListener('click', () =>{
+  document.querySelector("#photo-slider-wrapper").classList.add('visible');
+});
+
+function sendEmailInquiry(){
+
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let msg = document.querySelector("#message").value;
+
+  Email.send({
+    SecureToken : "2950b98d-6ac2-440e-ba48-39d78ad7c85e",
+    To : 'jhegypt@gmail.com',
+    From : 'jhegypt@gmail.com',
+    Subject : "뉴카이로하우스 - "+ name + "님께서 보내신 문의",
+    Body : "문의 내용 : " + msg + "<br>" + email + " 로 회신 바람."
+}).then(function(){
+  let successMsg = document.querySelector("#sent-msg");
+  successMsg.classList.add('pressed');
+  setTimeout(() => {
+    successMsg.classList.remove('pressed');
+  }, 10000)
+}
+);
+inquiryForm.reset();
+}
+
+// Copied from Slider.js
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+//Unitl here
